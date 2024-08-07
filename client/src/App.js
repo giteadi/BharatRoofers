33f2248ff4e4ddb3f7064f796287a9e8a2bed901
Aditya sharma <adityasharma10102000@gmail.com>
2024-08-07 18:17:56 +0530

    logic in hiddinen

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ function App() {
   return (
     <div>
       <UpperNav />
+    
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<ContactUs />} />
