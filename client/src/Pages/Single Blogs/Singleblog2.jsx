4d9f156b127d0a6f824670e1125e5c1649a4488f
Aditya sharma <adityasharma10102000@gmail.com>
2024-08-17 16:48:08 +0530

    final chande done...

--- client/src/Pages/Single Blogs/Singleblog2.jsx
+++ client/src/Pages/Single Blogs/Singleblog2.jsx
@@ -20,158 +20,150 @@ function Singleblog2() {
         <link rel="canonical" href={canonicalUrl} />
       </Helmet>
 
-      <div className="container mx-auto mt-5 px-4">
+      <div className="container mx-auto mt-5 px-4 lg:px-8">
         <div className="pb-4 border-b border-gray-300 text-center mt-4">
           <p className="inline-block p-4 text-lg">
             <span className="text-xl">
               <i className="bi bi-file-post"></i>
             </span>
-        
           </p>
-          <h1 className="text-3xl font-bold mt-4">
+          <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold mt-4">
             New Construction vs. Resale Homes: Making the Right Choice for Your Next Property
           </h1>
-          {/* <p className="mt-2 text-gray-600">
-            <small className="text-lg">
-              <span className="text-xl">
-                <CgCalendarDates />
-              </span> AUGUST 24, 2022
-            </small>
-          </p> */}
-          <div className="flex justify-center items-cetner gap-5 text-lg mt-5">
+          <div className="flex justify-center items-center gap-5 text-lg mt-5">
             <div className="pt-1"><CgCalendarDates /></div>
             <div>AUGUST 24, 2022</div>
           </div>
         </div>
 
-        <div className="mt-4">
+        <div className="mt-4 flex justify-center">
           <img
-             className="max-w-2xl h-auto mx-auto rounded-lg"
+            className="max-w-full md:max-w-md sm:w-sm  rounded-lg"
             src={blog2}
             alt="Blog"
           />
         </div>
         
         <div className="mt-5">
-          <h4 className="text-xl font-semibold">
+          <h4 className="text-lg md:text-xl font-semibold">
             In the quest for a new home, buyers often face the dilemma of choosing between new construction and resale properties. Each option comes with its own set of advantages and drawbacks, making the decision a crucial one in the home-buying journey.
           </h4>
         </div>
 
         <div className="mt-5">
-          <h3 className="text-2xl font-semibold">Pros and Cons of Buying New Construction Homes:</h3>
-          <h4 className="text-xl font-semibold mt-4">Pros:</h4>
+          <h3 className="text-xl md:text-2xl font-semibold">Pros and Cons of Buying New Construction Homes:</h3>
+          <h4 className="text-lg md:text-xl font-semibold mt-4">Pros:</h4>
 
-          <h5 className="text-lg font-semibold mt-4">Benefits of Customization and Modern Features:</h5>
+          <h5 className="text-base md:text-lg font-semibold mt-4">Benefits of Customization and Modern Features:</h5>
           <p className="text-gray-700 leading-relaxed mx-3">
             When you opt for a construction home, you can customize it according to your preferences. You can have a say in everything from the layout to the finishes, making it your dream home.
           </p>
-          <h5 className="text-lg font-semibold mt-4">Energy Efficiency for Savings and Sustainability:</h5>
+          <h5 className="text-base md:text-lg font-semibold mt-4">Energy Efficiency for Savings and Sustainability:</h5>
           <p className="text-gray-700 leading-relaxed mx-3">
             These built homes are equipped with energy-efficient technologies. This not only helps you save on utility bills but also reduces your carbon footprint, contributing to the environment.
           </p>
 
-          <h5 className="text-lg font-semibold mt-4">Warranty and Ease of Maintenance:</h5>
+          <h5 className="text-base md:text-lg font-semibold mt-4">Warranty and Ease of Maintenance:</h5>
           <p className="text-gray-700 leading-relaxed mx-3">
             Builders usually provide warranties that cover components of the home for a period. Moreover, new homes generally require less maintenance compared to older properties, allowing you to enjoy worry-free living.
           </p>
 
-          <h5 className="text-lg font-semibold mt-4">Enhanced Community Living:</h5>
+          <h5 className="text-base md:text-lg font-semibold mt-4">Enhanced Community Living:</h5>
           <p className="text-gray-700 leading-relaxed mx-3">
             Many new construction neighborhoods offer an array of amenities, like parks, pools, and gyms. These amenities foster a sense of community where residents can come together and enjoy shared spaces.
           </p>
         </div>
 
         <div className="mt-5">
-          <h4 className="text-xl font-semibold">Cons:</h4>
+          <h4 className="text-lg md:text-xl font-semibold">Cons:</h4>
 
-          <h5 className="text-lg font-semibold mt-4">Higher Price Tag:</h5>
+          <h5 className="text-base md:text-lg font-semibold mt-4">Higher Price Tag:</h5>
           <p className="text-gray-700 leading-relaxed mx-3">
             New construction homes often come at a premium compared to similar-sized resale properties in the same area.
           </p>
-          <h5 className="text-lg font-semibold mt-4">Completion Time:</h5>
+          <h5 className="text-base md:text-lg font-semibold mt-4">Completion Time:</h5>
           <p className="text-gray-700 leading-relaxed mx-3">
             Construction delays are possible, leading to extended wait times before moving in.
           </p>
 
-          <h5 className="text-lg font-semibold mt-4">Restricted Character and Mature Landscaping:</h5>
+          <h5 className="text-base md:text-lg font-semibold mt-4">Restricted Character and Mature Landscaping:</h5>
           <p className="text-gray-700 leading-relaxed mx-3">
             Recently constructed homes could lack the unique charm and established landscaping seen in more mature, resale properties.
           </p>
 
-          <h5 className="text-lg font-semibold mt-4">Potential for Unknown Development:</h5>
+          <h5 className="text-base md:text-lg font-semibold mt-4">Potential for Unknown Development:</h5>
           <p className="text-gray-700 leading-relaxed mx-3">
             Purchasing in a developing area could mean uncertainty about future improvements that could affect your lifestyle or property value.
           </p>
         </div>
 
         <div className="mt-5">
-          <h3 className="text-2xl font-semibold">Pros and Cons of Buying Resale Homes:</h3>
-          <h4 className="text-xl font-semibold mt-4">Pros:</h4>
+          <h3 className="text-xl md:text-2xl font-semibold">Pros and Cons of Buying Resale Homes:</h3>
+          <h4 className="text-lg md:text-xl font-semibold mt-4">Pros:</h4>
 
-          <h5 className="text-lg font-semibold mt-4">Established Neighborhoods:</h5>
+          <h5 className="text-base md:text-lg font-semibold mt-4">Established Neighborhoods:</h5>
           <p className="text-gray-700 leading-relaxed mx-3">
             Resale houses are typically located in established communities, with mature trees, beautifully landscaped surroundings, and a strong sense of community.
           </p>
-          <h5 className="text-lg font-semibold mt-4">Price Bargaining:</h5>
+          <h5 className="text-base md:text-lg font-semibold mt-4">Price Bargaining:</h5>
           <p className="text-gray-700 leading-relaxed mx-3">
             When it comes to resale homes, there is usually more flexibility to negotiate the price compared to new construction properties.
           </p>
 
-          <h5 className="text-lg font-semibold mt-4">Charm and History:</h5>
+          <h5 className="text-base md:text-lg font-semibold mt-4">Charm and History:</h5>
           <p className="text-gray-700 leading-relaxed mx-3">
             Older homes often showcase unique elements and carry a rich historical background that adds allure and character.
           </p>
 
-          <h5 className="text-lg font-semibold mt-4">Immediate Move-In:</h5>
+          <h5 className="text-base md:text-lg font-semibold mt-4">Immediate Move-In:</h5>
           <p className="text-gray-700 leading-relaxed mx-3">
             Unlike new constructions, resale homes are typically ready for occupancy right after purchase.
           </p>
         </div>
 
         <div className="mt-5">
-          <h4 className="text-xl font-semibold">Cons:</h4>
+          <h4 className="text-lg md:text-xl font-semibold">Cons:</h4>
 
-          <h5 className="text-lg font-semibold mt-4">Potential Maintenance:</h5>
+          <h5 className="text-base md:text-lg font-semibold mt-4">Potential Maintenance:</h5>
           <p className="text-gray-700 leading-relaxed mx-3">
             Older houses may need repairs or renovations in the future, which can increase overall expenses.
           </p>
-          <h5 className="text-lg font-semibold mt-4">Outdated Systems and Energy Inefficiency:</h5>
+          <h5 className="text-base md:text-lg font-semibold mt-4">Outdated Systems and Energy Inefficiency:</h5>
           <p className="text-gray-700 leading-relaxed mx-3">
             These homes might lack modern energy-efficient features and could have older systems that need upgrading.
           </p>
 
-          <h5 className="text-lg font-semibold mt-4">Limited Personalization:</h5>
+          <h5 className="text-base md:text-lg font-semibold mt-4">Limited Personalization:</h5>
           <p className="text-gray-700 leading-relaxed mx-3">
             Unlike new constructions, there might be limitations on customizing resale homes according to your specific preferences.
           </p>
 
-          <h5 className="text-lg font-semibold mt-4">Chance of Competitive Offers:</h5>
+          <h5 className="text-base md:text-lg font-semibold mt-4">Chance of Competitive Offers:</h5>
           <p className="text-gray-700 leading-relaxed mx-3">
             In competitive markets, buying a resale home could lead to bidding wars, driving up the final cost.
           </p>
         </div>
 
         <div className="mt-5">
-          <h4 className="text-xl font-semibold">FAQ’s</h4>
-          <h5 className="text-lg font-semibold mt-4">1. Which option tends to increase in value over time?</h5>
+          <h4 className="text-lg md:text-xl font-semibold">FAQ’s</h4>
+          <h5 className="text-base md:text-lg font-semibold mt-4">1. Which option tends to increase in value over time?</h5>
           <p className="text-gray-700 leading-relaxed mx-3">
             The appreciation in value relies on factors such as location, market trends, and the overall condition and appeal of the property.
           </p>
-          <h5 className="text-lg font-semibold mt-4">2. Is it possible to negotiate the price of a new home?</h5>
+          <h5 className="text-base md:text-lg font-semibold mt-4">2. Is it possible to negotiate the price of a new home?</h5>
           <p className="text-gray-700 leading-relaxed mx-3">
-            While there may be some room for negotiation, builders typically have less flexibility in pricing compared to sellers of pre-owned properties.
+            While there may be some room for negotiation, builders typically have less flexibility in pricing compared to individual sellers of resale homes.
           </p>
+          <h5 className="text-base md:text-lg font-semibold mt-4">3. Which option is better suited for first-time buyers?</h5>
+          <p className="text-gray-700 leading-relaxed mx-3">
+            Resale homes can be more budget-friendly, but new constructions offer the advantages of low maintenance and modern amenities. Ultimately, the choice depends on individual preferences and circumstances.
+          </p>
+        </div>
 
-          <h5 className="text-lg font-semibold mt-4">3. Are new homes more energy efficient?</h5>
-          <p className="text-gray-700 leading-relaxed mx-3">
-            Generally, new homes are designed with energy efficiency in mind; however, existing homes can also be retrofitted with energy-saving features.
-          </p>
-
-          <h5 className="text-lg font-semibold mt-4">4. Which option is better for investment purposes?</h5>
-          <p className="text-gray-700 leading-relaxed mx-3">
-            Both new homes and pre-owned properties can be good investments. Conducting market research and understanding your investment goals are essential for making a decision.
-          </p>
+        <div className="text-center mt-10">
+          <button className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-lg">
+            Contact Us for More Info
+          </button>
         </div>
       </div>
     </>
