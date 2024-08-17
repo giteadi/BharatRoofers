1abe8fade0051f63ae51d2f2f23fee2b9663eb9f
Aditya sharma <adityasharma10102000@gmail.com>
2024-08-17 16:19:42 +0530

    nav2 button

--- client/src/Pages/Nav2.jsx
+++ client/src/Pages/Nav2.jsx
@@ -150,7 +150,7 @@ const Nav2 = () => {
         </select>
         <button
           type="button"
-          className="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 border border-transparent transition-all duration-200 ease-in-out ml-auto"
+          className="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-4 py-2 border border-transparent transition-all duration-200 ease-in-out ml-auto md:w-[8rem] sm:w-[4rem]"
           onClick={handleSearch}
         >
           Search
